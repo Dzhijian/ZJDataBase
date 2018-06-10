e1e6f1e9503ba3077dc353499ba9fc239a208212
CodeDeng <dengzj@canplay.com.cn>
2018-06-11 01:06:33 +0800

    初始化项目

---
+++ ZJDataBase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJDataBase
+//
+//  Created by 邓志坚 on 2018/6/11.
+//  Copyright © 2018年 邓志坚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJDataBaseTests/ZJDataBaseTests.m
@@ -0,0 +1,39 @@
+//
+//  ZJDataBaseTests.m
+//  ZJDataBaseTests
+//
+//  Created by 邓志坚 on 2018/6/11.
+//  Copyright © 2018年 邓志坚. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZJDataBaseTests : XCTestCase
+
+@end
+
+@implementation ZJDataBaseTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
